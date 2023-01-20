b26d2d22adf77bfb1dfe53af283721867786688a
agustrinaldokurniawan <agustkurniawan010899@gmail.com>
2023-01-20 13:58:28 +0700

    finish

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from "native-base";
+
+import { light } from "./src/theme/light";
+import { Provider } from "react-redux";
+import { store } from "./src/redux/store";
+import Navigator from "./src/component/Navigator";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NativeBaseProvider theme={light}>
+        <Navigator />
+      </NativeBaseProvider>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
